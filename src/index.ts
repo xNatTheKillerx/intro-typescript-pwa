b9ecc1985ed9b93c0f06846a12eb4936c5f7ef85
xNatTheKillerx <19.gama.natalie.1d@gmail.com>
2022-09-10 09:42:16 -0500

    Primer typescript datos

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+
+
+console.log('Hola Mundo!');
+
+let nombre:string="Najimi";
+let cont:number=111;
+let acredita:boolean=false;
+
+let apaterno: number| string='Juan';
+apaterno=378;
+var miVariable:string='Hola';
+const tem=234;
+
+/*
+    ===== Código de TypeScript =====
+*/
+
